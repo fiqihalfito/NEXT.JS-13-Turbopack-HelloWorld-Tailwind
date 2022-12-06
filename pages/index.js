a560fc847084b65999e64b162361001b8db94357
fiqihalfito <fiqihalfito.dev@gmail.com>
2022-12-06 21:51:15 +0700

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl font-bold bg-amber-500 text-neutral-500">
+      Hello world!
+    </h1>
+  )
+}
